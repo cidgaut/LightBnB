1a0c2ab26aa533c0d7b941c8838cf9b0069cb7da
Cedric.gauthier <cidgaut@gmail.com>
2024-02-06 05:54:02 -0500

    added queries to LightBnB tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   email VARCHAR(255),
-  npassword VARCHAR(255)
+  password VARCHAR(255)
 );
 
 CREATE TABLE reservations (
