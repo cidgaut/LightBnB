bd1090be6dea62030b7114ff3c6865c3c7c3d42b
Cedric.gauthier <cidgaut@gmail.com>
2024-02-06 09:44:03 -0500

    added property function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -113,7 +113,7 @@ const getAllProperties = (options, limit = 10) => {
     FROM properties
     LEFT JOIN property_reviews ON properties.id = property_id
   `;
-  
+
   const filterClauses = [];
 
   if (options.owner_id) {
@@ -157,10 +157,52 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night * 100, // Convert to cents
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+
+  const queryString = `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    )
+    VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+    )
+    RETURNING *;
+  `;
+
+  return pool.query(queryString, queryParams)
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.error('Error executing query:', err.message);
+      throw err;
+    });
 };
 
 module.exports = {
